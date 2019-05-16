cc8efc45ed83bb1cc4ddd8fc881a0eab5749ae9f
Jurriaan van Hengel <j.vanhengel@dation.nl>
2019-05-16 15:12:35 +0200

    Apply form-table styling to admin page

--- admin/dation-woocommerce-admin.php
+++ admin/dation-woocommerce-admin.php
@@ -15,7 +15,8 @@ function dation_options_page() {
 		40
 	);
 }
-add_action( 'admin_menu', 'dation_options_page' );
+
+add_action('admin_menu', 'dation_options_page');
 
 /**
  * REgister a settings group for the plugin
@@ -23,29 +24,36 @@ add_action( 'admin_menu', 'dation_options_page' );
 function dw_register_settings() {
 	register_setting('dw_settings_group', 'dw_settings');
 }
+
 add_action('admin_init', 'dw_register_settings');
 
 /**
  * Display Admin page
  */
-function dation_options_page_html(){
+function dation_options_page_html() {
 	global $dw_options;
 
 	?>
 	<div class="wrap">
-		<h1><?php esc_html_e( get_admin_page_title() ); ?></h1>
+		<h1><?php esc_html_e(get_admin_page_title()); ?></h1>
 
 		<form method="post" action="options.php">
 			<?php settings_fields('dw_settings_group'); ?>
-			<p>
-				<label class="description" for="dw_settings[api_key]">
-					<?php _e('API-Key'); ?>
-				</label>
-				<input id="dw_settings[api_key]" name="dw_settings[api_key]"
-					   type="text"
-					   value="<?php echo $dw_options['api_key'] ?>"
-				>
-			</p>
+			<table class="form-table">
+				<tr>
+					<th scope="row">
+						<label class="description" for="dw_settings[api_key]">
+							<?php _e('API-Key'); ?>
+						</label>
+					</th>
+					<td>
+						<input id="dw_settings[api_key]" name="dw_settings[api_key]"
+							   type="text"
+							   value="<?php echo $dw_options['api_key'] ?>"
+						>
+					</td>
+				</tr>
+			</table>
 
 			<p class="submit">
 				<input type="submit" class="button-primary" value="<?php _e('Save') ?>">
