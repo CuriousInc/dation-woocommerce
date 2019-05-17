3c5e3814826df5022f3f4f3bc0d29238dcfb01cf
Jurriaan van Hengel <j.vanhengel@dation.nl>
2019-05-17 07:59:40 +0200

    Remove WC_Acme class

--- dation-woocommerce.php
+++ dation-woocommerce.php
@@ -17,67 +17,23 @@ $dw_options = get_option('dw_settings');
 // Includes
 
 /**
- * Check if WooCommerce is active
+ * Localisation
+ **/
+load_plugin_textdomain('dw', false, dirname(plugin_basename(__FILE__)) . '/');
+
+// called just before the woocommerce template functions are included
+add_action('init', 'dw_override_woo_templates', 20);
+
+// indicates we are running the admin
+if(is_admin()) {
+	require 'admin/dation-woocommerce-admin.php';
+	require 'admin/dation-woocommerce-get-products.php';
+}
+
+/**
+ * Override any of the template functions from woocommerce/woocommerce-template.php
+ * with our own template functions file
  */
-if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
-
-	if ( ! class_exists( 'WC_Acme' ) ) {
-
-		/**
-		 * Localisation
-		 **/
-		load_plugin_textdomain( 'wc_acme', false, dirname( plugin_basename( __FILE__ ) ) . '/' );
-
-		class WC_Acme {
-			public function __construct() {
-				// called only after woocommerce has finished loading
-				add_action( 'woocommerce_init', array( &$this, 'woocommerce_loaded' ) );
-
-				// called after all plugins have loaded
-				add_action( 'plugins_loaded', array( &$this, 'plugins_loaded' ) );
-
-				// called just before the woocommerce template functions are included
-				add_action( 'init', array( &$this, 'include_template_functions' ), 20 );
-
-				// indicates we are running the admin
-				if ( is_admin() ) {
-					require 'admin/dation-woocommerce-admin.php';
-					require 'admin/dation-woocommerce-get-products.php';
-				}
-
-				// indicates we are being served over ssl
-				if ( is_ssl() ) {
-					// ...
-				}
-
-				// take care of anything else that needs to be done immediately upon plugin instantiation, here in the constructor
-			}
-
-			/**
-			 * Take care of anything that needs woocommerce to be loaded.
-			 * For instance, if you need access to the $woocommerce global
-			 */
-			public function woocommerce_loaded() {
-				// ...
-			}
-
-			/**
-			 * Take care of anything that needs all plugins to be loaded
-			 */
-			public function plugins_loaded() {
-				// ...
-			}
-
-			/**
-			 * Override any of the template functions from woocommerce/woocommerce-template.php
-			 * with our own template functions file
-			 */
-			public function include_template_functions() {
-				include( 'includes/woocommerce-template.php' );
-			}
-		}
-
-		// finally instantiate our plugin class and add it to the set of globals
-		$GLOBALS['wc_acme'] = new WC_Acme();
-	}
+function dw_override_woo_templates() {
+	include('includes/woocommerce-template.php');
 }
