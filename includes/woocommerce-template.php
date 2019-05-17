63e106442f38dcc452866d376b6843b366443aa8
Jurriaan van Hengel <j.vanhengel@dation.nl>
2019-05-17 12:20:23 +0200

    Display custom checkout fields in order admin

--- includes/woocommerce-template.php
+++ includes/woocommerce-template.php
@@ -1,34 +1,40 @@
 <?php
+
 /**
  * Template Function Overrides
  *
  */
 
-// Hook in
-add_filter('woocommerce_checkout_fields', 'custom_override_checkout_fields');
+const ISSUE_DATE_DRIVING_LICENSE = 'issue_date_driving_license';
+const DATE_OF_BIRTH              = 'date_of_birth';
+const NATIONAL_REGISTRY_NUMBER   = 'rijksregisternummer';
+const AUTOMATIC_GEARS            = 'automatic_gears';
 
-// Our hooked in function - $fields is passed via the filter!
-function custom_override_checkout_fields($fields) {
+// Register override
+add_filter('woocommerce_checkout_fields', 'dw_override_checkout_fields');
 
-	$newOrderFields['order']['date_of_birth'] = [
+// Override checkout fields, add custom TKM fields to checkout fields
+function dw_override_checkout_fields($fields) {
+
+	$newOrderFields['order'][DATE_OF_BIRTH] = [
 		'type'     => 'text',
 		'label'    => __('Geboortedatum'),
 		'required' => true,
 	];
 
-	$newOrderFields['order']['rijksregisternummer'] = [
+	$newOrderFields['order'][NATIONAL_REGISTRY_NUMBER] = [
 		'type'     => 'text',
 		'label'    => __('Rijksregisternummer'),
 		'required' => true,
 	];
 
-	$newOrderFields['order']['issue_date_driving_license'] = [
+	$newOrderFields['order'][ISSUE_DATE_DRIVING_LICENSE] = [
 		'type'     => 'text',
 		'label'    => __('Afgiftedatum rijbewijs'),
 		'required' => true,
 	];
 
-	$newOrderFields['order']['automatic_gears'] = [
+	$newOrderFields['order'][AUTOMATIC_GEARS] = [
 		'type'     => 'select',
 		'options'  => [
 			'yes' => __('Yes'),
@@ -38,7 +44,58 @@ function custom_override_checkout_fields($fields) {
 		'required' => true,
 	];
 
+	// Merge arrays at the 'order' key
 	$fields['order'] = array_merge($newOrderFields['order'], $fields['order']);
 
 	return $fields;
+}
+
+/**
+ * Process the checkout
+ */
+add_action('woocommerce_checkout_process', 'dw_process_checkout');
+
+function dw_process_checkout() {
+
+	// // Check if set, if its not set add an error.
+	// if(!$_POST[DATE_OF_BIRTH]) {
+	// 	wc_add_notice(__('Geboorte 2 is compulsory. Please enter a value'), 'error');
+	// }
+
+}
+
+/**
+ * Update the order meta with field value
+ */
+add_action('woocommerce_checkout_update_order_meta', 'dw_checkout_update_order_meta');
+
+function dw_checkout_update_order_meta($order_id) {
+	$fields = [
+		ISSUE_DATE_DRIVING_LICENSE,
+		DATE_OF_BIRTH,
+		NATIONAL_REGISTRY_NUMBER,
+		AUTOMATIC_GEARS
+	];
+
+	foreach($fields as $field) {
+		if(!empty($_POST[$field])) {
+			update_post_meta($order_id, $field, sanitize_text_field($_POST[$field]));
+		}
+	}
+}
+
+/**
+ * Display field value on the order edit page
+ */
+add_action('woocommerce_admin_order_data_after_billing_address', 'dw_admin_order_tkm_data', 10, 1);
+
+function dw_admin_order_tkm_data($order) {
+	echo '<p><strong>' . __('Geboortedatum') . ':</strong> <br/>'
+		. get_post_meta($order->get_id(), DATE_OF_BIRTH, true) . '</p>';
+	echo '<p><strong>' . __('Rijksregisternummer') . ':</strong> <br/>'
+		. get_post_meta($order->get_id(), NATIONAL_REGISTRY_NUMBER, true) . '</p>';
+	echo '<p><strong>' . __('Afgiftedatum rijbewijs') . ':</strong> <br/>'
+		. get_post_meta($order->get_id(), ISSUE_DATE_DRIVING_LICENSE, true) . '</p>';
+	echo '<p><strong>' . __('Automaat') . ':</strong> <br/>'
+		. get_post_meta($order->get_id(), AUTOMATIC_GEARS, true) . '</p>';
 }
