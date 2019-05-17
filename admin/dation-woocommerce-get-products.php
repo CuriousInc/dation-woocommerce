0041f1301d3185e1ba4f655dc5fc01de358799e3
Dirk Huethorst <d.huethorst@dation.nl>
2019-05-17 12:30:22 +0200

    Add added products table WP-style

--- admin/dation-woocommerce-get-products.php
+++ admin/dation-woocommerce-get-products.php
@@ -8,54 +8,43 @@ const VARIABLES = [
 	'low_stock_amount' => 0,
 ];
 
-function dw_get_products() {
-	global $dw_options;
-	$curl = curl_init();
 
-	$addedProducts = 0;
+date_default_timezone_set('Europe/Amsterdam');
+add_action('dw_delete_past_products', 'delete_past_products');
+
+// WP_List_Table is not loaded automatically so we need to load it in our application
+if( ! class_exists( 'WP_List_Table' ) ) {
+	require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+}
+
+function dw_get_products() {
+	echo '<h1>Cursussen</h1>';
 
 	$today = new DateTime();
-	curl_setopt_array($curl, array(
-		CURLOPT_URL => "https://dashboard.dation.nl/api/v1/course-instances?startDateAfter=" . $today->format('Y-m-d'),
-		CURLOPT_RETURNTRANSFER => true,
-		CURLOPT_ENCODING => "",
-		CURLOPT_MAXREDIRS => 10,
-		CURLOPT_TIMEOUT => 30,
-		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		CURLOPT_CUSTOMREQUEST => "GET",
-		CURLOPT_HTTPHEADER => array(
-			"Accept: */*",
-			'Authorization: Basic ' . $dw_options['api_key'],
-			"Cache-Control: no-cache",
-			"Connection: keep-alive",
-			"Host: dashboard.dation.nl",
-			"Postman-Token: a80eff2e-473e-4dc0-a88d-10e692a54d27,48664008-cb2b-4d9b-aece-f101f633d26b",
-			"User-Agent: PostmanRuntime/7.13.0",
-			"accept-encoding: gzip, deflate",
-			"cache-control: no-cache",
-			"cookie: PHPSESSID=03d87add6fd48ecdd289e00d061592f5"
-		),
-	));
-
-	$response = curl_exec($curl);
-	$err = curl_error($curl);
-
-	curl_close($curl);
+	$courses = get_course_instances($today, null);
 
 	$createdProducts = [];
 
-	if ($err) {
-		echo "cURL Error #:" . $err;
-	} else {
-		foreach(json_decode($response, true) as $dationProduct) {
-			if(get_product_by_sku($dationProduct['id']) === null) {
-				$addedProducts += 1;
-				$product = add_woo_commerce_product($dationProduct);
-				$createdProducts[] = $product;
-			}
+	foreach($courses as $dationProduct) {
+		if(get_product_by_sku($dationProduct['id']) === null) {
+			$product = add_woo_commerce_product($dationProduct);
+			$createdProducts[] = $product;
 		}
 	}
-	generateTable($createdProducts);
+
+	if(count($createdProducts) > 0 ) {
+		echo '<p>Er zijn ' . count($createdProducts) . 'cursussen gesynchroniseerd met dation</p>';
+	}
+
+
+	$table = new DationProductList();
+	$table->prepare_items();
+	?>
+	<div class="wrap">
+		<div id="icon-users" class="icon32"></div>
+		<?php $table->display(); ?>
+	</div>
+	<?php
 }
 
 function add_woo_commerce_product($dationProduct) {
@@ -87,7 +76,7 @@ function add_woo_commerce_product($dationProduct) {
 
 	$product = new WC_Product();
 
-	$product->set_name($dationProduct['name'] . ' ' . $date->format('d-m-Y'));
+	$product->set_name($dationProduct['name'] . ' ' . date_i18n('l d F Y H:i', $date->getTimestamp()));
 	$product->set_description($dationProduct['name']);
 	$product->set_short_description($dationProduct['ccvCode']);
 	$product->set_sku($dationProduct['id']);
@@ -122,26 +111,120 @@ function get_product_by_sku( $sku ) {
 	return null;
 }
 
-function generateTable(array $createdProducts) {
-	?>
-	<p>De volgende Dation producten zijn toegevoegd aan de webshop</p>
-	<table>
-		<thead>
-			<tr>
-				<th>Dation Product</th>
-				<th>Webshop Product</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php
-				foreach($createdProducts as $product) {
-					echo '<tr>
-							<td><a href="https://dashboard.dation.nl/eqmobi/nascholing/details?id='. $product->get_sku() . '">'. $product->get_sku() . '</a></td>
-							<td><a href="https://www.mygenerationdrive.be/wp-admin/post.php?post='. $product->get_id() . '&action=edit">'. $product->get_name() . '</a></td>
-						</tr>';
-				}
-			?>
-		</tbody>
-	</table>
-	<?php
+function get_course_instances(DateTime $startDateAfter = null, DateTime $startDateBefore = null) {
+	global $dw_options;
+
+	$beforeParam = $startDateBefore ? '&startDateBefore=' . $startDateBefore->format('Y-m-d') : '';
+	$afterParam = $startDateAfter ? '&startDateAfter=' . $startDateAfter->format('Y-m-d') : '';
+
+	$curl = curl_init();
+
+	curl_setopt_array($curl, array(
+		CURLOPT_URL => "https://dashboard.dation.nl/api/v1/course-instances?startDateAfter=" . $beforeParam . $afterParam,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => "",
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 30,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => "GET",
+		CURLOPT_HTTPHEADER => array(
+			"Accept: */*",
+			'Authorization: Basic ' . $dw_options['api_key'],
+			"Cache-Control: no-cache",
+			"Connection: keep-alive",
+			"Host: dashboard.dation.nl",
+			"Postman-Token: a80eff2e-473e-4dc0-a88d-10e692a54d27,48664008-cb2b-4d9b-aece-f101f633d26b",
+			"User-Agent: PostmanRuntime/7.13.0",
+			"accept-encoding: gzip, deflate",
+			"cache-control: no-cache",
+			"cookie: PHPSESSID=03d87add6fd48ecdd289e00d061592f5"
+		),
+	));
+
+	$response = curl_exec($curl);
+	$err = curl_error($curl);
+
+	curl_close($curl);
+
+	if ($err) {
+		echo "cURL Error #:" . $err;
+	} else {
+		return json_decode($response, true);
+	}
+
+	return null;
+}
+
+
+class DationProductList extends WP_List_Table {
+	public function prepare_items() {
+		$columns = $this->get_columns();
+		$hidden = $this->get_hidden_columns();
+		$sortable = $this->get_sortable_columns();
+
+		$data = $this->table_data();
+
+		$perPage = 25;
+		$currentPage = $this->get_pageNum();
+		$totalItems = count($data);
+
+		$this->set_pagination_args([
+			'total_items' => $totalItems,
+			'per_page' => $perPage
+		]);
+
+		$data = array_slice($data, (($currentPage -1) * $perPage), $perPage);
+
+		$this->_column_headers = [$columns, $hidden, $sortable];
+		$this->items = $data;
+	}
+
+	public function get_columns() {
+		return [
+			'id'       => 'Webshop Product',
+			'location' => 'Locatie',
+			'sku'      => 'Dation Product',
+		];
+	}
+
+	public function get_hidden_columns() {
+		return [];
+	}
+
+	public function get_sortable_columns() {
+		return [
+			[
+				'id',
+				true,
+			]
+		];
+	}
+
+	private function table_data() {
+		$query = new WC_Product_Query();
+		$products = $query->get_products();
+		$data = [];
+		/** @var WC_Product $product */
+		foreach($products as $product) {
+			$data[] = [
+				'sku'      => $product->get_sku(),
+				'id'       => $product->get_id(),
+				'name'     => $product->get_name(),
+				'location' => $product->get_attribute('pa_locatie'),
+			];
+		}
+
+		return $data;
+	}
+
+	public function column_default($item, $column_name) {
+		switch ($column_name) {
+			case 'sku':
+				return '<a target="_blank" href="https://dashboard.dation.nl/eqmobi/nascholing/details?id='. $item[$column_name]. '">Open in Dation</a>';
+			case 'id':
+				return '<a target="_blank" href="https://www.mygenerationdrive.be/wp-admin/post.php?post='. $item[$column_name] . '&action=edit">'. $item['name'] . '</a>';
+			case 'location':
+				return $item[$column_name];
+		}
+	}
 }
