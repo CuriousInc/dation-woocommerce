7afbe27926474ea3558fa75faf050235c5fbba02
Jorgen Kesseler <jkesseler@gmail.com>
2020-02-24 15:57:32 +0100

    Fixes for date time input.

--- contact-form/src/Components/DateInput/date-input.js
+++ contact-form/src/Components/DateInput/date-input.js
@@ -1,52 +1,63 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import DateTime from 'react-datetime';
 import moment from 'moment';
 import icons from 'glyphicons';
-import styles from'./date-input.css';
+
+import 'moment/locale/nl';
+import './date-input.css';
+
 // This component is only usable in combination with Bootstrap Style loader.
-const DateInput = ({label = 'datepicker', initialValue, name, wrapperClasses, labelClasses, inputClasses}) => {
-	const [value, setValue] = useState(initialValue);
+const DateInput = ({
+  label = 'datepicker', initialValue, name, wrapperClasses, labelClasses, inputClasses,
+}) => {
+  const [value, setValue] = useState(initialValue);
 
-	const handleChange = inputValue => {
-		if(moment.isMoment(inputValue)) {
-			setValue(inputValue.format('DD-MM-YYYY'))
-		}
-	};
+  const handleChange = (inputValue) => {
+    if (moment.isMoment(inputValue)) {
+      setValue(inputValue.format('DD-MM-YYYY'));
+    }
+  };
 
-	return (
-		<div className={wrapperClasses}>
-			<label className={labelClasses}>{label}</label>
-			<div className='input-group'>
-				<DateTime
-					dateFormat={'DD-MM-YYYY'}
-					timeFormat={false}
-					closeOnSelect
-					locale='nl'
-					value={value}
-					inputProps={{
-						name: {name},
-					}}
-					className={inputClasses}
-					onChange={() => {
-					}}
-				/>
-				<div className='input-group-append'>
-					<span className='input-group-text' onClick={() => console.error('This should open the datepicker')}>
-						{icons.calendar}
-					</span>
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div className={wrapperClasses}>
+      <label className={labelClasses}>{label}</label>
+      <div className="input-group">
+        <DateTime
+          dateFormat="DD-MM-YYYY"
+          timeFormat={false}
+          closeOnSelect
+          locale="nl"
+          value={value}
+          inputProps={{
+            name,
+          }}
+          className={inputClasses}
+          onChange={handleChange}
+        />
+        <div className="input-group-append">
+          <span className="input-group-text">
+            {icons.calendar}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 DateInput.propTypes = {
-	label: PropTypes.string.isRequired,
-	name: PropTypes.string.isRequired,
-	initialValue: PropTypes.string,
-	wrapperClasses: PropTypes.string,
-	labelClasses: PropTypes.string,
-	inputClasses: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  initialValue: PropTypes.string,
+  wrapperClasses: PropTypes.string,
+  labelClasses: PropTypes.string,
+  inputClasses: PropTypes.string,
+};
+
+DateInput.defaultProps = {
+  initialValue: '',
+  wrapperClasses: '',
+  labelClasses: '',
+  inputClasses: '',
 };
 export default DateInput;
