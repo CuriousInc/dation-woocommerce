2758218cfaefd50def1df831e46ce04479cf4432
Jorgen Kesseler <jkesseler@gmail.com>
2020-02-28 12:44:34 +0100

    [DD-1243] Add example external props

--- contact-form/webpack.build-config.js
+++ contact-form/webpack.build-config.js
@@ -3,7 +3,18 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const appMountId = 'app';
+
+const bodyHtmlSnippet = `
+<script type="application/json" data-props="${appMountId}">
+{
+  "location": "Amsterdam"
+}
+</script>
+`;
+
 const config = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -77,8 +88,9 @@ const config = {
     new HtmlWebpackPlugin({
       template: require('html-webpack-template'),
       inject: false,
-      appMountId: 'app',
+      appMountId,
       filename: 'index.html',
+      bodyHtmlSnippet,
     }),
   ],
 };
