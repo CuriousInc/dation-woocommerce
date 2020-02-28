ccb73211180514e277e054b4b1ac7d00c4b558dd
Jorgen Kesseler <jkesseler@gmail.com>
2020-02-28 12:20:54 +0100

    [DD-1243] Allow external props via scripttags

--- contact-form/src/index.js
+++ contact-form/src/index.js
@@ -2,7 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-const element = document.getElementById('app');
 
-// todo: get data from url (trainingId, trainingName, date, location)
-ReactDOM.render(<App title="MP06 Defensief rijden - thema 1 - C" date="02-03-2020" location="Alkmaar" trainingId="66" />, element);
+const { name } = document.currentScript.dataset || {};
+const dataNode = document.querySelector(`[data-props=${name}]`);
+const externalProps = dataNode ? JSON.parse(dataNode.textContent) : {};
+
+const props = {
+  title: 'MP06 Defensief rijden - thema 1 - C',
+  date: '02-03-2020',
+  location: 'Alkmaar',
+  trainingId: 66,
+  ...externalProps,
+};
+
+const element = document.getElementById('app');
+ReactDOM.render(<App {...props} />, element);
+
+
+/**
+ * Html snippet
+ * `type="application/json"` is required to stop browsers frm trtying to execute the plain JSON
+ */
+
+/*
+    <script src="path/to/bundle.js" data-name="BundleName" crossorigin />
+    <script type="application/json" data-props="BundleName">
+      {
+        "title": "Nieuwe title",
+        "date": "01-01-2021",
+      }
+    </script>
+*/
